5d0bdcd5d2bd0df2731d7d5b41cf35fa4e504778
Igor Czajkowski <igor.czajkowski@j-labs.pl>
2021-07-12 22:02:11 +0200

    Add page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'list',
+    pathMatch: 'full',
+  },
+  {
+    path: 'list',
+    loadChildren: () =>
+      import('./pokemon-list').then((m) => m.PokemonListModule),
+  },
+  {
+    path: 'pageNotFound',
+    loadChildren: () => import('./not-found').then((m) => m.NotFoundModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'pageNotFound'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
